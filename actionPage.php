1eeadbf081c9d0a4b65b6c0c0e859930627241eb
ph4rden <70247533+ph4rden@users.noreply.github.com>
2021-11-07 14:31:52 -0600

    Add files via upload

---
+++ actionPage.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name']; 
+    $emailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "brianphan17@yahoo.com";
+    $headers = "From: ".emailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message; 
+
+    mail($mailTo, $subject, $txt, $headers); 
+    
+
+
+}
